c033508d5091d8df0271894d1e05ba46268d8b21
shamssar <nayefshams3@gmail.com>
2022-04-27 15:12:51 +0300

    add 404 and 500

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const port = 3000
 app.get("/", handleHomePage); 
     app.get("/favorite", handleFavorite);
 
-
-    function handleHomePage(req, res) {   
-        let newMovie = new movie(dataJson.title, dataJson.poster_path, dataJson.overview);
-        res.json(newMovie);
-}
+    app.get('*',notFoundHandler);
+    app.get(handleServerError) ;
+    
+    
+     function handleHomePage(req, res) {   
+         let newMovie = new movie(dataJson.title, dataJson.poster_path, dataJson.overview);
+   res.json(newMovie);
+ }
 
 function handleFavorite(req, res) {
     res.send("Welcome to Favorite Page");
@@ -29,6 +32,18 @@ function movie(title,poster_path,overview){
     this.title=title,
     this.poster_path=poster_path,
     this.overview=overview
+}
+
+
+function notFoundHandler(req,res){
+    res.status(404).send("This page is not found")
+ }
+ function handleServerError (Error,request,response){                      
+    const err = {
+        status : 500,
+        message : Error
+    };
+    response.status(500).send(err);
 }
 
 
